8325304b34b8f63f231693ef8c8a91882763df49
33kou <okke.3wa6ra@ezweb.ne.jp>
2018-04-11 14:26:00 +0900

    ライントレースのサンプルコードを追加

--- SampleCode/ev3Traning2/src/ev3Traning/Traning.java
+++ SampleCode/ev3Traning2/src/ev3Traning/Traning.java
@@ -2,7 +2,9 @@ package ev3Traning;
 
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.port.TachoMotorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
@@ -12,8 +14,8 @@ import lejos.utility.Delay;
 
 public class Traning {
 
-	static RegulatedMotor MotorR = Motor.B;//ポートBに右のモーターがつながっている
-	static RegulatedMotor MotorL = Motor.C;//ポートCに左のモーターがつながっている
+	static TachoMotorPort MotorR = MotorPort.B.open(TachoMotorPort.class);;//ポートBに右のモーターがつながっている
+	static TachoMotorPort MotorL = MotorPort.C.open(TachoMotorPort.class);;//ポートCに左のモーターがつながっている
 	static EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S2);
 	//使用するセンサーを定義する
 	//タッチセンサーはS2ポートにつながっているためこのように定義する
@@ -24,9 +26,131 @@ public class Traning {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		init();
+		setTSensor();
+		while(CheckTSensor() == false)
+		{
+			
+		}
+		while(true)
+		{
+			//onOffLinetrace(0.5F,50);
+			//PIDLinetrace(200.0F,10.0F,1.0F,0.5F,50);
+			
+			/*
+			lcdclear();
+			drawLCD(""+MotorR.getTachoCount(),1,1);
+			if(MotorR.getTachoCount() < 100)
+			{
+				PIDLinetrace(50.0F,5.0F,1.0F,0.5F,100);
+			}
+			else if(MotorR.getTachoCount() < 600)
+			{
+				PIDLinetrace(100.0F,10.0F,1.0F,0.5F,50);
+			}
+			else if(MotorR.getTachoCount() < 1000)
+			{
+				PIDLinetrace(50.0F,10.0F,5.0F,0.5F,100);
+			}
+			else
+			{
+				PIDLinetrace(200.0F,10.0F,1.0F,0.5F,50);
+			}
+			*/
+		}
+		
+	}
+	private static float brightness;
+
+	//輝度値基準値。本番環境で調整必要
+	private final static float BLACK_BRIGHTNESS = 0.01F;
+	private final static float WHITE_BRIGHTNESS = 0.52F;
+
+	//正規化後の最大・最小値
+	private final static float BRIGHTNESS_MAX = 1.0F;
+	private final static float BRIGHTNESS_MIN = 0.0F;
+
+	public static float getNormalizedBrightness(){
+		float normalizedBrightness = (getBrightness()-BLACK_BRIGHTNESS)/(WHITE_BRIGHTNESS-BLACK_BRIGHTNESS);
+
+		return Math.min(Math.max(normalizedBrightness, BRIGHTNESS_MIN),BRIGHTNESS_MAX);
+	}
+
+	public static float getBrightness(){
+		SensorMode Red = colorSensor.getRedMode();
+		float[] sampleBright = new float[Red.sampleSize()];
+		float a = 0.8F;//大きくすると遅れるが滑らかになる
+
+		Red.fetchSample(sampleBright, 0);
+		//出力値 = a*ひとつ前の出力値 + (1-a)*センサ値
+		//ひとつ前の出力値はbrightnessとなっている筈
+		brightness = a*brightness + (1-a)*sampleBright[0];
+		return brightness;
+	}
+	
+	 public static void onOffLinetrace(float target_brightness,float target_forward){
+		float brightness = getNormalizedBrightness();
+		float turn = 0.0F;
+		if(brightness - target_brightness > 0)
+		{
+			turn = 50.0F;
+		}
+		else
+		{
+			turn = -50.0F;
+		}
+
+		controlWheels(turn,target_forward);
 		
 	}
 	
+	private static float currentDiff = 0.0F;
+	private static float preDiff = 0.0F;
+	private static float integral = 0.0F;
+	private final static float DELTA_T = 0.004F;
+	
+	 public static void PIDLinetrace(float kp,float ki,float kd,float target_brightness,float target_forward){
+		float brightness = getNormalizedBrightness();
+
+		preDiff = currentDiff;
+		currentDiff = brightness - target_brightness;
+		integral += ((currentDiff + preDiff) / 2.0F)*DELTA_T;
+
+		float turn = kp * currentDiff;
+		turn += ki * integral;
+		turn += kd * (currentDiff - preDiff)/DELTA_T;
+
+		controlWheels(turn,target_forward);
+		
+	}
+	 
+	 public static void controlWheels(float turn,float forward){
+			//計算式は適当
+			//turnのMAXを50(-50)の想定のとき
+			//外輪と内輪のパワー差が２倍になる感じ
+			int powerL;
+			int powerR;
+			if(turn >= 0){
+				powerL = (int)((100-turn)/100 * forward);
+			}
+			else{
+				powerL = (int)forward;
+			}
+			if(turn <= 0){
+				powerR = (int)((100+turn)/100 * forward);
+			}
+			else{
+				powerR = (int)forward;
+			}
+
+			controlWheelsDirect(powerL,powerR);
+		}
+	 public static void controlWheelsDirect(int powerL,int powerR){
+
+			MotorL.controlMotor(powerL, 1);
+			MotorR.controlMotor(powerR, 1);
+		}
+	 
+	 
 	//LCD上に表示されている文字をすべてクリアします．
 		private static void lcdClear(){
 			LCD.clear();
@@ -49,12 +173,14 @@ public class Traning {
 		//モーターを初期化する関数
 		static void init(){
 			MotorR.resetTachoCount();
-			MotorR.rotateTo(0);
+			for(int i = 0; i < 1500;i++){
+				MotorL.controlMotor(0, 1);
+				MotorR.controlMotor(0, 1);
+			}
 			MotorL.resetTachoCount();
-			MotorL.rotateTo(0);
 		}
 
-
+/*
 		//モーターを動かす関数
 		//右と左の速度を引数として受けとって、モーターを動かす
 		static void setMotor(int Right, int Left){
@@ -69,7 +195,7 @@ public class Traning {
 			else if(Left<0)MotorL.backward();
 			else MotorL.stop();
 		}
-
+*/
 		//指定した秒数だけ待機する関数
 		static void Delaysec(double d){
 			Delay.msDelay((int)(d*1000));
